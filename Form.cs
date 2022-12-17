de2a8267019ce7320435a4736a71d1a4a65c4ab1
Vheos <Vheos@VHEOS-PC>
2022-12-17 06:41:02 +0100

    Add project files.

---
+++ Form.cs
@@ -0,0 +1,11 @@
+namespace Vheos.Tools.FilePatcher
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
