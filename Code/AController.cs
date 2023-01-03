8e58a4a8b099239c0a13239f7e04b4cf59c09ec6
Vheos <Vheos777@gmail.com>
2023-01-03 22:17:54 +0100

    Added `PatchController`

---
+++ Code/AController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vheos.Tools.FilePatcher.Controls;
+
+namespace Vheos.Tools.FilePatcher.Code
+{
+    public abstract class AController<TModel, TView>
+    {
+        public readonly TModel Model;
+        public readonly TView View;
+
+        public AController(TModel model, TView view)
+        {
+            Model = model;
+            View = view;
+        }
+    }
+}
---
+++ Code/PatchController.cs
@@ -0,0 +1,25 @@
+﻿using Vheos.Tools.FilePatcher.Controls;
+
+namespace Vheos.Tools.FilePatcher.Code
+{
+    internal class PatchController : AController<PatchModel, PatchView>
+    {
+        public PatchController(PatchModel model, PatchView view) : base(model, view)
+        { }
+
+        async public Task Initialize(string name)
+        {
+            View.Name = name;
+
+            View.LoadProgress = 0.0f;
+            while(View.LoadProgress < 1f)
+            {
+                View.LoadProgress += 0.1f;
+                await Task.Delay(Random.Shared.Next(100, 500));
+            }
+
+            View.CanBeDisabled = Model.PresetsByName.ContainsKey(string.Empty);
+            View.Presets = Model.PresetsByName.Keys.Where(name => name.IsNotEmpty());
+        }
+    }
+}
