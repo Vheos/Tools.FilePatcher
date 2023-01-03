08f76da0125f75e9ea5a3169d25c7d3cd8ea6f44
Vheos <Vheos777@gmail.com>
2023-01-03 22:18:20 +0100

    Updated UI

--- Forms/Form.Designer.cs
+++ Forms/Form.Designer.cs
@@ -28,34 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this._panel = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
-            // label1
+            // _panel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(443, 156);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
-            this.label1.Click += new System.EventHandler(this.label_Click);
+            this._panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._panel.Location = new System.Drawing.Point(0, 0);
+            this._panel.Name = "_panel";
+            this._panel.Size = new System.Drawing.Size(427, 242);
+            this._panel.TabIndex = 2;
             // 
             // Form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(427, 242);
+            this.Controls.Add(this._panel);
+            this.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form";
             this.Text = "Form";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private Label label1;
+        private Panel _panel;
     }
 }
