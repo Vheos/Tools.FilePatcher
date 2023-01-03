b3c438d0780c148704983261e01d1a9f384c876c
Vheos <Vheos777@gmail.com>
2023-01-03 22:17:45 +0100

    Added struct `Settings`

---
+++ Code/Settings.cs
@@ -0,0 +1,9 @@
+﻿namespace Vheos.Tools.FilePatcher.Code
+{
+    public readonly struct Settings
+    {
+        public string PatchFileExtension { get; init; }
+        public string PatchFileDirectory { get; init; }
+        public string[] VanillaPresetAliases { get; init; }
+    }
+}
