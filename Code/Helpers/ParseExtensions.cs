3d0c2f295193a959246271e0a5133e8c84e4bd36
Vheos <Vheos777@gmail.com>
2023-01-04 03:29:16 +0100

    Refactored

--- Code/Helpers/ParseExtensions.cs
+++ Code/Helpers/ParseExtensions.cs
@@ -9,49 +9,49 @@ public static partial class ParseExtensions
 {
     public static bool TryToByte(this string @this, out byte value, ParseBase parseBase = ParseBase.Decimal)
     {
-        InitializeArgs(parseBase, ref @this, out NumberStyles format);
+        parseBase.InitializeArgs(ref @this, out NumberStyles format);
         return byte.TryParse(@this, format, CultureInfo.InvariantCulture, out value);
     }
 
     public static bool TryToSByte(this string @this, out sbyte value, ParseBase parseBase = ParseBase.Decimal)
     {
-        InitializeArgs(parseBase, ref @this, out NumberStyles format);
+        parseBase.InitializeArgs(ref @this, out NumberStyles format);
         return sbyte.TryParse(@this, format, CultureInfo.InvariantCulture, out value);
     }
 
     public static bool TryToShort(this string @this, out short value, ParseBase parseBase = ParseBase.Decimal)
     {
-        InitializeArgs(parseBase, ref @this, out NumberStyles format);
+        parseBase.InitializeArgs(ref @this, out NumberStyles format);
         return short.TryParse(@this, format, CultureInfo.InvariantCulture, out value);
     }
 
     public static bool TryToUShort(this string @this, out ushort value, ParseBase parseBase = ParseBase.Decimal)
     {
-        InitializeArgs(parseBase, ref @this, out NumberStyles format);
+        parseBase.InitializeArgs(ref @this, out NumberStyles format);
         return ushort.TryParse(@this, format, CultureInfo.InvariantCulture, out value);
     }
 
     public static bool TryToInt(this string @this, out int value, ParseBase parseBase = ParseBase.Decimal)
     {
-        InitializeArgs(parseBase, ref @this, out NumberStyles format);
+        parseBase.InitializeArgs(ref @this, out NumberStyles format);
         return int.TryParse(@this, format, CultureInfo.InvariantCulture, out value);
     }
 
     public static bool TryToUInt(this string @this, out uint value, ParseBase parseBase = ParseBase.Decimal)
     {
-        InitializeArgs(parseBase, ref @this, out NumberStyles format);
+        parseBase.InitializeArgs(ref @this, out NumberStyles format);
         return uint.TryParse(@this, format, CultureInfo.InvariantCulture, out value);
     }
 
     public static bool TryToLong(this string @this, out long value, ParseBase parseBase = ParseBase.Decimal)
     {
-        InitializeArgs(parseBase, ref @this, out NumberStyles format);
+        parseBase.InitializeArgs(ref @this, out NumberStyles format);
         return long.TryParse(@this, format, CultureInfo.InvariantCulture, out value);
     }
 
     public static bool TryToULong(this string @this, out ulong value, ParseBase parseBase = ParseBase.Decimal)
     {
-        InitializeArgs(parseBase, ref @this, out NumberStyles format);
+        parseBase.InitializeArgs(ref @this, out NumberStyles format);
         return ulong.TryParse(@this, format, CultureInfo.InvariantCulture, out value);
     }
 
@@ -72,109 +72,89 @@ public static partial class ParseExtensions
 
     public static byte ToByte(this string @this, ParseBase parseBase = ParseBase.Decimal)
     {
-        if (@this.TryToByte(out byte value, parseBase))
-            return value;
-
-        Debug.WriteLine($"Cannot parse {nameof(Byte)}: \"{@this}\"!");
-        return default;
+        if (!@this.TryToByte(out byte value, parseBase))
+            @this.LogWarning<byte>();
+        return value;
     }
     public static sbyte ToSByte(this string @this, ParseBase parseBase = ParseBase.Decimal)
     {
-        if (@this.TryToSByte(out sbyte value, parseBase))
-            return value;
-
-        Debug.WriteLine($"Cannot parse {nameof(SByte)}: \"{@this}\"!");
-        return default;
+        if (!@this.TryToSByte(out sbyte value, parseBase))
+            @this.LogWarning<sbyte>();
+        return value;
     }
 
     public static int ToInt(this string @this, ParseBase parseBase = ParseBase.Decimal)
     {
-        if (@this.TryToInt(out int value, parseBase))
-            return value;
-
-        Debug.WriteLine($"Cannot parse {nameof(Int32)}: \"{@this}\"!");
-        return default;
+        if (!@this.TryToInt(out int value, parseBase))
+            @this.LogWarning<int>();
+        return value;
     }
     public static uint ToUInt(this string @this, ParseBase parseBase = ParseBase.Decimal)
     {
-        if (@this.TryToUInt(out uint value, parseBase))
-            return value;
-
-        Debug.WriteLine($"Cannot parse {nameof(UInt32)}: \"{@this}\"!");
-        return default;
+        if (!@this.TryToUInt(out uint value, parseBase))
+            @this.LogWarning<uint>();
+        return value;
     }
 
     public static long ToLong(this string @this, ParseBase parseBase = ParseBase.Decimal)
     {
-        if (@this.TryToLong(out long value, parseBase))
-            return value;
-
-        Debug.WriteLine($"Cannot parse {nameof(Int64)}: \"{@this}\"!");
-        return default;
+        if (!@this.TryToLong(out long value, parseBase))
+            @this.LogWarning<long>();
+        return value;
     }
     public static ulong ToULong(this string @this, ParseBase parseBase = ParseBase.Decimal)
     {
-        if (@this.TryToULong(out ulong value, parseBase))
-            return value;
-
-        Debug.WriteLine($"Cannot parse {nameof(UInt64)}: \"{@this}\"!");
-        return default;
+        if (!@this.TryToULong(out ulong value, parseBase))
+            @this.LogWarning<ulong>();
+        return value;
     }
 
     public static float ToFloat(this string @this)
     {
-        if (@this.TryToFloat(out float value))
-            return value;
-
-        Debug.WriteLine($"Cannot parse {nameof(Single)}: \"{@this}\"!");
-        return default;
+        if (!@this.TryToFloat(out float value))
+            @this.LogWarning<float>();
+        return value;
     }
     public static double ToDouble(this string @this)
     {
-        if (@this.TryToDouble(out double value))
-            return value;
-
-        Debug.WriteLine($"Cannot parse {nameof(Double)}: \"{@this}\"!");
-        return default;
+        if (!@this.TryToDouble(out double value))
+            @this.LogWarning<double>();
+        return value;
     }
 
     public static bool ToBool(this string @this)
     {
-        if (@this.TryToBool(out bool value))
-            return value;
-
-        Debug.WriteLine($"Cannot parse {nameof(Boolean)}: \"{@this}\"!");
-        return default;
+        if (!@this.TryToBool(out bool value))
+            @this.LogWarning<bool>();
+        return value;
     }
     public static BigInteger ToBigInteger(this string @this)
     {
-        if (@this.TryToBigInteger(out BigInteger value))
-            return value;
-
-        Debug.WriteLine($"Cannot parse {nameof(BigInteger)}: \"{@this}\"");
-        return default;
+        if (!@this.TryToBigInteger(out BigInteger value))
+            @this.LogWarning<BigInteger>();
+        return value;
     }
     public static T ToEnum<T>(this string @this, bool ignoreCase = false) where T : struct, Enum
     {
-        if (@this.TryToEnum(out T value, ignoreCase))
-            return value;
-
-        Debug.WriteLine($"Cannot parse {nameof(T)}: \"{@this}\"!");
-        return default;
+        if (!@this.TryToEnum(out T value, ignoreCase))
+            @this.LogWarning<T>();
+        return value;
     }
 
-    private static void InitializeArgs(ParseBase parseBase, ref string @this, out NumberStyles format)
+    private static void InitializeArgs(this ParseBase @this, ref string parseText, out NumberStyles format)
     {
-        switch (parseBase)
+        switch (@this)
         {
             case ParseBase.Hexadecimal:
-            case ParseBase.Auto when @this.HasHexPrefix() || @this.HasHexLetter():
+            case ParseBase.Auto when parseText.HasHexPrefix() || parseText.HasHexLetter():
                 format = NumberStyles.HexNumber;
-                @this = @this.WithoutHexPrefix();
+                parseText = parseText.WithoutHexPrefix();
                 break;
             default:
                 format = NumberStyles.Integer;
                 break;
         }
     }
+    private static void LogWarning<T>(this string @this)
+        => Debug.WriteLine($"Cannot parse \"{@this}\" as {typeof(T).Name}! Returning default value ({default(T)})");
 }
