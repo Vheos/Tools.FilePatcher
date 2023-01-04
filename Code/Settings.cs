39b3cf47ab5e5d5ffd5e3fce1707c135b121d4f9
Vheos <Vheos777@gmail.com>
2023-01-04 03:40:22 +0100

    Added deserializable `PatcherSettings`

--- Code/Settings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Vheos.Tools.FilePatcher.Code;
-
-public readonly struct Settings
-{
-    public string PatchFileExtension { get; init; }
-    public string PatchFileDirectory { get; init; }
-    public string[] VanillaPresetAliases { get; init; }
-}
--- Forms/Form.cs
+++ Forms/Form.cs
@@ -2,25 +2,23 @@ using System.Text.Json;
 using Vheos.Tools.FilePatcher.Code;
 using Vheos.Tools.FilePatcher.Code.Helpers;
 using Vheos.Tools.FilePatcher.Controls;
+using Vheos.Helpers.Collections;
 
 namespace Vheos.Tools.FilePatcher;
 
 public partial class Form : System.Windows.Forms.Form
 {
-    private readonly Settings Settings = new()
-    {
-        PatchFileExtension = "patch",
-        PatchFileDirectory = @"./Patches",
-        VanillaPresetAliases = new[] { "Vanilla", "Default", "Off" },
-    };
+    private static readonly string ExecutableFileName = Path.GetFileNameWithoutExtension(Application.ExecutablePath);
+    private const string PatcherSettingsExtension = "settings";
 
     public Form()
     {
         InitializeComponent();
         InitializeJsonOptions();
+        PatcherSettings patcherSettings = InitializePatcherSettings();
 
         int counter = 0;
-        foreach ((string name, PatchModel model) in DeserializePatchFiles(Settings))
+        foreach ((string name, PatchModel model) in LoadPatchFiles(patcherSettings))
         {
             PatchView view = new(_panel);
             view.Location = new Point(0, view.Height * counter++);
@@ -37,15 +35,19 @@ public partial class Form : System.Windows.Forms.Form
         JsonUtil.Options.ReadCommentHandling = JsonCommentHandling.Skip;
     }
 
-    private static IEnumerable<KeyValuePair<string, PatchModel>> DeserializePatchFiles(Settings settings)
+    private static PatcherSettings InitializePatcherSettings()
     {
-        foreach (string file in Directory.GetFiles(settings.PatchFileDirectory, $"*.{settings.PatchFileExtension}", SearchOption.AllDirectories))
-        {
-            if (File.ReadAllText(file).TryDeserialize(out Dictionary<string, PatchModel.Json> jsonPatches))
-            {
+        FileInfo file = new($"{ExecutableFileName}.{PatcherSettingsExtension}");
+        return(file.TryReadText(out var text) && text.TryDeserialize(out PatcherSettings.Json settingsJson))
+            ? new PatcherSettings(settingsJson)
+            : PatcherSettings.Default;
+    }
+
+    private static IEnumerable<KeyValuePair<string, PatchModel>> LoadPatchFiles(PatcherSettings settings)
+    {
+        foreach (FileInfo file in settings.GetAllPatchFiles())
+            if (file.TryReadText(out var text) && text.TryDeserialize(out Dictionary<string, PatchModel.Json> jsonPatches))
                 foreach ((string name, PatchModel.Json patch) in jsonPatches)
                     yield return new(name, new PatchModel(patch, settings.VanillaPresetAliases));
-            }
-        }
     }
 }
