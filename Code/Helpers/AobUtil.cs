41218d34cda9d6e9129c6cab892ce0cb2eb865e5
Vheos <Vheos777@gmail.com>
2023-01-05 07:35:29 +0100

    Added `byte[]` extension `Compare()`

--- Code/Helpers/AobUtil.cs
+++ Code/Helpers/AobUtil.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 using Vheos.Tools.FilePatcher.Code.Enums;
 using ValueType = Vheos.Tools.FilePatcher.Code.Enums.ValueType;
@@ -26,6 +27,34 @@ public static class AobUtil
         }
     }
 
+    public static byte[] AOB(this byte @this) => new byte[] { @this };
+    public static byte[] AOB(this sbyte @this) => new byte[] { (byte)@this };
+    public static byte[] AOB(this ushort @this) => BitConverter.GetBytes(@this);
+    public static byte[] AOB(this short @this) => BitConverter.GetBytes(@this);
+    public static byte[] AOB(this uint @this) => BitConverter.GetBytes(@this);
+    public static byte[] AOB(this int @this) => BitConverter.GetBytes(@this);
+    public static byte[] AOB(this ulong @this) => BitConverter.GetBytes(@this);
+    public static byte[] AOB(this long @this) => BitConverter.GetBytes(@this);
+    public static byte[] AOB(this float @this) => BitConverter.GetBytes(@this);
+    public static byte[] AOB(this double @this) => BitConverter.GetBytes(@this);
+
+    public static bool Compare(this byte[] @this, byte[] other, bool requreExactLength = false)
+    {
+        if (@this == other)
+            return true;
+        if (@this == null || other == null)
+            return false;
+        if (requreExactLength && @this.Length != other.Length)
+            return false;
+
+        var length = @this.Length.Min(other.Length);
+        for (int i = 0; i < length; i++)
+            if (@this[i] != other[i])
+                return false;
+
+        return true;
+    }
+
     private static IEnumerable<byte> ParseTextElement(string text, ValueType type) => type switch
     {
         ValueType.Hex => text.HexAOB(),
@@ -44,22 +73,6 @@ public static class AobUtil
     private static string HexDigits(this string @this) => Regex.Replace(@this, @"[^0-9a-f]+", "", RegexOptions.IgnoreCase);
     private static string[] SplitTextElements(this string @this) => Regex.Replace(@this, @"[^a-z0-9+\-.]+", "|", RegexOptions.IgnoreCase).Split("|");
 
-    private static byte[] AOB(this byte @this) => new byte[] { @this };
-    private static byte[] AOB(this sbyte @this) => new byte[] { (byte)@this };
-    private static byte[] AOB(this ushort @this) => BitConverter.GetBytes(@this);
-    private static byte[] AOB(this short @this) => BitConverter.GetBytes(@this);
-    private static byte[] AOB(this uint @this) => BitConverter.GetBytes(@this);
-    private static byte[] AOB(this int @this) => BitConverter.GetBytes(@this);
-    private static byte[] AOB(this ulong @this) => BitConverter.GetBytes(@this);
-    private static byte[] AOB(this long @this) => BitConverter.GetBytes(@this);
-    private static byte[] AOB(this float @this) => BitConverter.GetBytes(@this);
-    private static byte[] AOB(this double @this) => BitConverter.GetBytes(@this);
-    private static byte[] AOB<T>(this T[] @this)
-    {
-        byte[] bytes = new byte[@this.Length * Unsafe.SizeOf<T>()];
-        Buffer.BlockCopy(@this, 0, bytes, 0, bytes.Length);
-        return bytes;
-    }
     public static byte[] HexAOB(this string @this)
     {
         string[] hexes = @this.HexDigits().SplitBySize(2, true).ToArray();
