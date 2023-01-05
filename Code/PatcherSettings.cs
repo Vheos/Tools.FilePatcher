239227cbe9fb30b788157c73d8fadbd0b28887a7
Vheos <Vheos777@gmail.com>
2023-01-05 07:38:45 +0100

    Replaced `""` with `string.Empty`

--- Code/PatcherSettings.cs
+++ Code/PatcherSettings.cs
@@ -34,7 +34,7 @@ public class PatcherSettings
             PatchExtension = "patch",
             PatchesDirectory = @"./Patches",
             IncludeSubdirectories = "true",
-            VanillaPresetAliases = new[] { "", "Vanilla", "Default", "Off" },
+            VanillaPresetAliases = new[] { string.Empty, "Vanilla", "Default", "Off" },
         };
 
         public Json()
