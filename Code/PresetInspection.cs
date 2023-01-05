1df6d18af1e550ecbd1d247003f7b32a7ffde8af
Vheos <Vheos777@gmail.com>
2023-01-05 07:36:06 +0100

    Added struct `PresetInspection`

---
+++ Code/PresetInspection.cs
@@ -0,0 +1,17 @@
+﻿using Vheos.Tools.FilePatcher.Code.Enums;
+
+namespace Vheos.Tools.FilePatcher.Code;
+
+public readonly struct PresetInspection
+{
+    public readonly PresetType Type;
+    public readonly byte[] AOB;
+    public readonly string Name;
+
+    public PresetInspection(PresetType type, byte[] aob, string name)
+    {
+        Type = type;
+        Name = name;
+        AOB = aob;
+    }
+}
