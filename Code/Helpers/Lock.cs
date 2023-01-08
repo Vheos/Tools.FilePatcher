9c0169cc6815bfd1178caccea15eeb8e033e82dd
Vheos <Vheos777@gmail.com>
2023-01-08 21:19:02 +0100

    Added disposable `Lock`

---
+++ Code/Helpers/Lock.cs
@@ -0,0 +1,10 @@
+﻿namespace Vheos.Tools.FilePatcher.Code.Helpers;
+public class Lock : IDisposable
+{
+    private byte _locks = 0;
+    public bool Active => _locks > 0;
+    public void Initialize() => _locks++;
+    public void Dispose() => _locks--;
+
+    public static implicit operator bool(Lock @this) => @this.Active;
+}
